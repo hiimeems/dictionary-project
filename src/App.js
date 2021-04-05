0f98342ece3c2bda66069e4afe202139ff84ae5c
himeems <michelle@michelles-MacBook-Pro.local>
2021-04-04 18:59:01 -0700

    Building search engine for Dictionary

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
@@ -8,6 +9,12 @@ export default function App() {
         <header className="App-header">
           <img src={logo} className="App-logo img-fluid" alt="logo" />
         </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>coded by SheCodes</small>
+        </footer>
       </div>
     </div>
   );
